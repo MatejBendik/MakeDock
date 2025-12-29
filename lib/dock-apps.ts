45e502bf747f548d7153081e13ad2a9761199d07
MatejBendik <matejbendik.mb@gmail.com>
2025-12-29 23:57:16 +0100

    Add new icons

--- lib/dock-apps.ts
+++ lib/dock-apps.ts
@@ -61,6 +61,26 @@ export const availableApps: DockApp[] = [
     icon: 'https://cdn.jim-nielsen.com/macos/1024/safari-2021-06-02.png?rf=1024'
   },
   {
+    id: 'arc',
+    name: 'Arc',
+    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/da3fa63b53f5b5b9088727ea895aa685_low_res_Arc__dark_.png'
+  },
+  {
+    id: 'chrome',
+    name: 'Google Chrome',
+    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/eedbff3ef5fbf0baa52daf0cdd0d6505_low_res_chrome.png'
+  },
+  {
+    id: 'brave',
+    name: 'Brave',
+    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/8fd272f2a048aee02d1ed1f29160540a_low_res_Brave.png'
+  },
+  {
+    id: 'firefox',
+    name: 'Firefox',
+    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/80d190f77cb54771164da933f9485c5a_low_res_Firefox__Dark___macOS_26.2__.png'
+  },
+  {
     id: 'photos',
     name: 'Photos',
     icon: 'https://cdn.jim-nielsen.com/macos/1024/photos-2021-05-28.png?rf=1024'
@@ -116,6 +136,11 @@ export const availableApps: DockApp[] = [
     icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/880bf964f1352d71af45f1893717e030_low_res_Visual_Studio_Code.png'
   },
   {
+    id: 'ghostty',
+    name: 'Ghostty',
+    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/3fb391f668c626f75541ecf86496bb89_low_res_Ghostty__Liquid_Glass_.png'
+  },
+  {
     id: 'spotify',
     name: 'Spotify',
     icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/2966a8081bf2a5de2e69f29bb1915822_low_res_Spotify___Liquid_Glass__Dark_.png'
@@ -136,13 +161,8 @@ export const availableApps: DockApp[] = [
     icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/cf6442ea9f5040ff8569d35cda178ae6_low_res_Notion.png'
   },
   {
-    id: 'chrome',
-    name: 'Google Chrome',
-    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/eedbff3ef5fbf0baa52daf0cdd0d6505_low_res_chrome.png'
-  },
-  {
-    id: 'firefox',
-    name: 'Firefox',
-    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/80d190f77cb54771164da933f9485c5a_low_res_Firefox__Dark___macOS_26.2__.png'
+    id:'notioncalendar',
+    name: 'Notion Calendar',
+    icon: 'https://parsefiles.back4app.com/JPaQcFfEEQ1ePBxbf6wvzkPMEqKYHhPYv8boI1Rc/abf5445f59685b2541f36ce8226e0cc0_low_res_Notion_Calendar.png'
   }
 ];
